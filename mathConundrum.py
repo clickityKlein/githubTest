d1ea8016ae12343922d55ef37cd4508295b56508
clickityKlein <90531710+clickityKlein@users.noreply.github.com>
2022-04-06 23:03:47 -0700

    Added "mathConundrum.py"

---
+++ mathConundrum.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Question:
+Use 1, 3, 4, & 6 to equal 24 using any of the basic mathematical operations.
+
+1) Find the solution
+2) Expand to finding solution(s) to any 4 variables, with any constant
+3) Expand to finding solution(s) to any n variables, with any constant
+    
+"""
+
+x = 1
+y = 3
+z = 4
+t = 6
+
+c = 24
+
