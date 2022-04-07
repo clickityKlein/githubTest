d6bc562b943de9f9eb7003127014f4306df42770
clickityKlein <90531710+clickityKlein@users.noreply.github.com>
2022-04-06 23:08:22 -0700

    Add files via upload

--- mathConundrum.py
+++ mathConundrum.py
@@ -7,6 +7,7 @@ Use 1, 3, 4, & 6 to equal 24 using any of the basic mathematical operations.
 2) Expand to finding solution(s) to any 4 variables, with any constant
 3) Expand to finding solution(s) to any n variables, with any constant
     
+Test Changes Here!
 """
 
 x = 1
